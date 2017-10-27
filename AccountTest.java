1c027db26d187c2bb98092e206c412d648aa8aff
lmaschma <32856520+lmaschma@users.noreply.github.com>
2017-10-26 21:24:26 -0500

    Account Test completed.

---
+++ AccountTest.java
@@ -0,0 +1,41 @@
+
+package projectatm;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author laurarogers
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+    
+
+    /**
+     * Test of login method, of class Account.
+     */
+    @Test
+    public void testLogin1() {
+        System.out.println("login method tests empty string user input");
+        String accNo = "";
+        String pinNo = "";
+        assertEquals("Invalid entries should return false",false, Account.login(accNo, pinNo));
+    }
+    
+    @Test
+    public void testLogin2() {
+        System.out.println("login method tests incorrect PIN");
+        String accNo = "12345";
+        String pinNo = "11111";
+        assertEquals("Invalid entries should return false",false, Account.login(accNo, pinNo));
+    }
+
+    
+}
